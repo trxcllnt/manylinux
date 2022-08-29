780c6e0f8d484a30cbdb4b18338ae05b27a302bd
Sevag Hanssian <shanssian@nvidia.com>
2022-08-28 23:39:49 -0400

    Combine some commands

--- docker/build_scripts/install-rapids-tools.sh
+++ docker/build_scripts/install-rapids-tools.sh
@@ -15,11 +15,9 @@ curl -o /tmp/sccache.tar.gz \
         chmod +x /usr/bin/sccache
 
 export UCX_VERSION=1.13.0
-mkdir -p /ucx-src /opt/ucx &&\
-        cd /ucx-src &&\
+mkdir -p /ucx-src /opt/ucx && cd /ucx-src &&\
         git clone https://github.com/openucx/ucx -b v${UCX_VERSION} ucx-git-repo &&\
-        cd ucx-git-repo &&\
-        ./autogen.sh && ./contrib/configure-release --prefix=/opt/ucx &&\
+        cd ucx-git-repo && ./autogen.sh && ./contrib/configure-release --prefix=/opt/ucx &&\
         make && make install && cd /opt/ucx && rm -rf /ucx-src/
 
 # Install gha-tools v0.0.02
